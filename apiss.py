2ca9f52dce327b669d177720eba8b16cf317f381
GUVI-Courses <123951681+GUVI-Courses@users.noreply.github.com>
2023-06-20 10:48:39 +0530

    Add files via upload

---
+++ apiss.py
@@ -0,0 +1,14 @@
+# pip install requests
+# import requests
+# api = requests.get("https://arkprocoder.com/api/")
+# print(api)
+# print(api.content)
+
+# https://api.github.com/users
+
+# import requests
+# api = requests.get("https://api.github.com/users")
+# print(api)
+# print(api.content)
+# print(api.json())
+# print(api.status_code)
