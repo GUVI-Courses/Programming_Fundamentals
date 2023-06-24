3f23b4d0c63cb523e54a50e184af91f08ed43f50
GUVI-Courses <123951681+GUVI-Courses@users.noreply.github.com>
2023-06-24 10:40:11 +0530

    Add files via upload

---
+++ datatypes (1).py
@@ -0,0 +1,81 @@
+# single line comment  
+'''
+print("data types")
+print("data types")
+print("data types")
+print("data types")
+
+multi line comments
+'''
+
+print("Data types")
+'''
+# strings
+name="guvi"
+print(name)
+
+
+firstName="Anees"
+lastName='khan'
+print(firstName,lastName)
+print(type(firstName))
+print(type(lastName))
+
+'''
+
+'''
+# intergers
+
+a=12
+b=12.2587
+c=a+b
+print(c)
+print(type(a))
+print(type(b))
+'''
+
+'''
+# lists or arrays
+
+mylist= ["apple","orange",1,2,4.5]
+print(mylist)
+print(type(mylist))
+
+'''
+
+# sets
+s={1,2,34}
+print(s)
+print(type(s))
+
+# boolean 
+
+isActive=True
+isAvailable=False
+print(isActive,isAvailable)
+print(type(isActive))
+
+# dictionarys
+
+details={
+    "name":"guvi",
+    "since":2014,
+    "courses":["python",'java'],
+    "isActive":True
+}
+print(details)
+print(type(details))
+
+
+# none
+
+a=None
+a=12
+print(a)
+print(type(a))
+
+# tuples
+
+mytup=(1,2,3,4)
+print(type(mytup))
+print(mytup)
